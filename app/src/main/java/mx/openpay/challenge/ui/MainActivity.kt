4bfbb12652bff49e508699b19280b80608518707
IrvinggLara <warriorsdevv@gmail.com>
2023-06-15 11:14:05 -0700

    create first fragment add navigation and view binding

--- app/src/main/java/mx/openpay/challenge/ui/MainActivity.kt
+++ app/src/main/java/mx/openpay/challenge/ui/MainActivity.kt
@@ -2,11 +2,16 @@ package mx.openpay.challenge.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import mx.openpay.challenge.R
+import mx.openpay.challenge.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
